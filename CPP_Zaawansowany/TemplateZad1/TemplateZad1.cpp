43cfdf23d9d5676777ba4e9cef7627eb3bd40a0a
Michal Orlikowski <michalorlik@gmail.com>
2022-07-14 20:22:34 +0200

    Zad2

--- CPP_Zaawansowany/TemplateZad1/TemplateZad1.cpp
+++ CPP_Zaawansowany/TemplateZad1/TemplateZad1.cpp
@@ -21,6 +21,26 @@ T* max(T* a, T* b)
     return *a > *b ? a : b;
 }
 
+template<>
+char* max(char* a, char* b)
+{
+    if (strcmp(a, b) > 0)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+template<typename T> 
+T max(T* data, const unsigned int n)
+{
+    T tmpMax = data[0]; 
+    for()
+}
+
 int main()
 {
     int num1 = 100;
@@ -31,4 +51,10 @@ int main()
     int* ptr1 = &num1;
     int* ptr2 = &num2;
     std::cout << "Wiekszy ptr: " << *max(ptr1, ptr2) << std::endl;
+
+    std::cout << "Wiekszy ³anuch: " << max("AAAzaabbba", "AAAccc") << std::endl;
+
+    const unsigned int size = 10;
+    int tab[size] = { 0, 412, 123, 12, 32, 11, 23, 56, 1, 555 };
+    std::cout << "Najwiekszy element w tablicy:" << max(tab, size) << std::endl;
 }
