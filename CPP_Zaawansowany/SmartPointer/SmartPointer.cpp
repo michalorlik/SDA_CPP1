7cbcf02639abca7fcf15de94f4a3aa78a9c1dd64
Michal Orlikowski <michalorlik@gmail.com>
2022-07-19 20:27:18 +0200

    Prezentacja operatorow

--- CPP_Zaawansowany/SmartPointer/SmartPointer.cpp
+++ CPP_Zaawansowany/SmartPointer/SmartPointer.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include "SimpleSmartPointer.h"
 
+struct Foo
+{
+    Foo(int in) : a(in) {}
+    void print() const
+    {
+        std::cout << "a = " << a << '\n';
+    }
+    int a;
+};
+
 int main()
 {
     SimpleSmartPointer<int> ptr(new int(7));
 
     std::cout << "Wartosc pod wskaznikiem: " << *ptr;
+
+    SimpleSmartPointer<Foo> ptrFoo(new Foo(666));
+    ptrFoo->print();
+    (*ptrFoo).print();
 }
