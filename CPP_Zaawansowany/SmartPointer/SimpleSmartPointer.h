0edc59041e67b1440fde656594ecc29945945e41
Michal Orlikowski <michalorlik@gmail.com>
2022-07-19 19:49:40 +0200

    Smart pointer do dokonczenia

---
+++ CPP_Zaawansowany/SmartPointer/SimpleSmartPointer.h
@@ -0,0 +1,29 @@
+#pragma once
+
+class SimpleSmartPointer
+{
+public:
+	SimpleSmartPointer(int* data)
+	{
+
+	}
+
+	~SimpleSmartPointer()
+	{
+
+	}
+
+	?? operator*()
+	{
+
+	}
+
+	int* operator->()
+	{
+
+	}
+
+private:
+	int* _data;
+};
+
---
+++ CPP_Zaawansowany/SmartPointer/SmartPointer.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "SimpleSmartPointer.h"
+
+int main()
+{
+    SimpleSmartPointer ptr(new int(7));
+
+    std::cout << "Wartosc pod wskaznikiem: " << * ptr;
+}
