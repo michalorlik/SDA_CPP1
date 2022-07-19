7cf34c91476ffa69a150906e2d6c6c34125bb340
Michal Orlikowski <michalorlik@gmail.com>
2022-07-19 20:19:32 +0200

    SimpleSmartPointer bez szablonu

--- CPP_Zaawansowany/SmartPointer/SimpleSmartPointer.h
+++ CPP_Zaawansowany/SmartPointer/SimpleSmartPointer.h
@@ -4,23 +4,25 @@ class SimpleSmartPointer
 {
 public:
 	SimpleSmartPointer(int* data)
+		: _data(data)
 	{
-
+		//_data = data;
 	}
 
 	~SimpleSmartPointer()
 	{
-
+		delete _data;
+		// _data = nullptr; //tutaj nie potrzeba bo po delete juz obiekt naszej klasy nie istnieje i nie ma mozliwosci odwolac sie do taj pamieci niechcacy
 	}
 
-	?? operator*()
+	int operator*()
 	{
-
+		return *_data;
 	}
 
 	int* operator->()
 	{
-
+		return _data;
 	}
 
 private:
--- CPP_Zaawansowany/SmartPointer/SmartPointer.cpp
+++ CPP_Zaawansowany/SmartPointer/SmartPointer.cpp
@@ -5,5 +5,5 @@ int main()
 {
     SimpleSmartPointer ptr(new int(7));
 
-    std::cout << "Wartosc pod wskaznikiem: " << * ptr;
+    std::cout << "Wartosc pod wskaznikiem: " << *ptr;
 }
