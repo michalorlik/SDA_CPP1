0ff3df228fd2521e7f9e1909e8630b156bf0275a
Michal Orlikowski <michalorlik@gmail.com>
2022-07-21 19:19:53 +0200

    Smart pointer zad 1

---
+++ CPP_Zaawansowany/SmartPtrZad1/SmartPtrZad1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <memory>
+
+class Chicken
+{
+public:
+
+    Chicken()
+    {
+        std::cout << "Chicken" << std::endl;
+    }
+
+    ~Chicken()
+    {
+        std::cout << "~Chicken" << std::endl;
+    }
+};
+
+void fun()
+{
+    Chicken* rawPtr = new Chicken();
+
+    std::unique_ptr<Chicken> smartPtr(new Chicken());
+
+    delete rawPtr;
+}
+
+int main()
+{
+    fun();
+    std::cout << "Hello World!\n";
+}
+
