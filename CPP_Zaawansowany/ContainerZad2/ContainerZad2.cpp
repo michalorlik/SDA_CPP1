05a15a140b8de27f836844883a0a845016826631
Michal Orlikowski <michalorlik@gmail.com>
2022-07-23 13:17:01 +0200

    Containers

---
+++ CPP_Zaawansowany/ContainerZad2/ContainerZad2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <list>
+
+//template<typename T>
+//void print(const std::list<T>& list) {
+//    for (size_t i = 0; i < list.size() - 1; ++i)
+//    {
+//        std::cout << "[" << i << "]=" << vec.at(i) << ";\n";
+//    }
+//}
+
+int main()
+{
+    std::list<int> list1 = { 1,2,3,4,5,6,7,8,9,10 };
+    std::list<int> list2{ 11,12,13,14,15,16,17,18,19,20 };
+    //splice 
+    std::cout << "Hello World!\n";
+}
