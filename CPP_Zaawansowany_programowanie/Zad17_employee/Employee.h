2012ecfa41cc6ef4cbeee91ca3f10730455b4336
Michal Orlikowski <michalorlik@gmail.com>
2022-09-08 19:07:51 +0200

    Wczytywanie do plikow

--- CPP_Zaawansowany_programowanie/Zad17_employee/Employee.h
+++ CPP_Zaawansowany_programowanie/Zad17_employee/Employee.h
@@ -11,7 +11,46 @@ public:
 		UNKNOWN
 	};
 
-	//Dodac gettery/settery/c-tor
+	static Gender toGender(std::string str)
+	{
+		if (str == "Female")
+		{
+			return Gender::FEMALE;
+		}
+		else if (str == "Male")
+		{
+			return Gender::MALE;
+		}
+		else
+		{
+			return Gender::UNKNOWN;
+		}
+	}
+
+	void setFirstName(const std::string name)
+	{
+		firstName = name;
+	}
+
+	void setLastName(const std::string name)
+	{
+		lastName = name;
+	}
+
+	void setEmail(const std::string mail)
+	{
+		email = mail;
+	}
+
+	void setGender(const Gender gender)
+	{
+		this->gender = gender;
+	}
+
+	void setSalary(const double salary)
+	{
+		this->salary = salary;
+	}
 
 private:
 
--- CPP_Zaawansowany_programowanie/Zad17_employee/EmployeeManager.cpp
+++ CPP_Zaawansowany_programowanie/Zad17_employee/EmployeeManager.cpp
@@ -1,4 +1,11 @@
 #include "EmployeeManager.h"
+#include <memory>
+
+EmployeeManager::EmployeeManager(std::shared_ptr<IEmployeeFileReader> fileReader, std::shared_ptr<IEmployeeFileWriter> fileWriter)
+	: _fileReader(fileReader)
+	, _fileWriter(fileWriter)
+{
+};
 
 void EmployeeManager::readDataFromFile(std::string filePath)
 {
